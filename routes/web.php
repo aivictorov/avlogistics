d4c2e0e98076b2f27a36d0ec0f4bcda5ad9a735d
aivictorov <aivictorov@yandex.ru>
2024-03-22 18:41:39 +0300

    22.03.2024_2

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/admin_panel', function () {
+    return view('admin.home.index'); 
+});
+
