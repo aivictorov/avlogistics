88127200ade1061be7239671e05728b453324232
aivictorov <aivictorov@yandex.ru>
2024-05-24 00:05:14 +0300

    23.05.2024_8

--- resources/views/site/sections/main-map.blade.php
+++ resources/views/site/sections/main-map.blade.php
@@ -1,10 +1,11 @@
 <section class="main-map">
     <div class="container">
-        <div class="zhd">
-            <h1 class="zhd__big">ЖД перевозки</h1>
-            <div class="zhd__row">
-                <span class="zhd__accent">грузов в вагонах и контейнерах</span>
-                <span class="zhd__main"> из любой точки России, СНГ, Китая</span>
+        <div class="main-map__content">
+            <h1 class="main-map__title">ЖД перевозки</h1>
+            <div class="main-map__subtitle">
+                <a class="main-map__subtitle-primary" href="{{ route('pages.show', ['page' => 'zhd-perevozki']) }}">грузов
+                    в вагонах и контейнерах</a>
+                <span class="main-map__subtitle-secondary"> из любой точки России, СНГ, Китая</span>
             </div>
         </div>
     </div>
