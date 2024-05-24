9d5c09f5128e354c8ef72dfeceb74b5d62cc348b
aivictorov <aivictorov@yandex.ru>
2024-05-24 13:37:07 +0300

    24.05.2024_2

--- resources/views/site/sections/main-map.blade.php
+++ resources/views/site/sections/main-map.blade.php
@@ -1,11 +1,19 @@
 <section class="main-map">
     <div class="container">
         <div class="main-map__content">
-            <h1 class="main-map__title">ЖД перевозки</h1>
+            {{-- <h1 class="main-map__title">ЖД перевозки</h1> --}}
+            <h1 class="main-map__title">Перевозки грузов</h1>
             <div class="main-map__subtitle">
-                <a class="main-map__subtitle-primary" href="{{ route('pages.show', ['page' => 'zhd-perevozki']) }}">грузов
-                    в вагонах и контейнерах</a>
-                <span class="main-map__subtitle-secondary"> из любой точки России, СНГ, Китая</span>
+                {{-- <a class="main-map__subtitle-primary" href="{{ route('pages.show', ['page' => 'zhd-perevozki']) }}">
+                    грузов в вагонах и контейнерах
+                </a> --}}
+                <a class="main-map__subtitle-primary" href="{{ route('pages.show', ['page' => 'zhd-perevozki']) }}">
+                    железнодорожным
+                </a>
+                {{-- <span class="main-map__subtitle-secondary"> из любой точки России, СНГ, Китая</span> --}}
+                <span class="main-map__subtitle-secondary">, автомобильным, морским транспортом <br> из любой точки
+                    России,
+                    СНГ, Китая</span>
             </div>
         </div>
     </div>
