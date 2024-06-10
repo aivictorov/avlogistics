c69c356330a1c3838a488fb6158debfcdf6f8db0
aivictorov <aivictorov@yandex.ru>
2024-06-10 16:23:49 +0300

    10.06.2024_1

--- resources/views/site/layouts/main.blade.php
+++ resources/views/site/layouts/main.blade.php
@@ -6,9 +6,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
+    <!-- SEO -->
     <title>@yield('title')</title>
     <meta name="Description" content="@yield('description')" />
     <meta name="Keywords" content="@yield('keywords')" />
+    <link rel="canonical" href="{{ url()->current() }}" />
 
     <!-- Styles -->
     <link rel="stylesheet" href="/assets/swiper/swiper-bundle.min.css">
