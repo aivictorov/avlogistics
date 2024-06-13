8a630f1ed9ce6f524a4a12521243875eab24279e
aivictorov <aivictorov@yandex.ru>
2024-06-13 12:36:25 +0300

    13.06.2024_1

--- resources/views/site/sections/main-map.blade.php
+++ resources/views/site/sections/main-map.blade.php
@@ -1,22 +1,20 @@
 <section class="main-map">
     <div class="container">
         <div class="main-map__content">
-            {{-- <h1 class="main-map__title">ЖД перевозки</h1> --}}
-            <h1 class="main-map__title">
-                <span class="main-map__title-primary">Перевозки грузов</span>
-                <a class="main-map__title-secondary main-map__title-secondary--accent"
-                    href="{{ route('pages.show', ['page' => 'zhd-perevozki']) }}">
-                    железнодорожным</a><span class="main-map__title-secondary">, автомобильным, морским
-                    транспортом</span><span class="main-map__title-secondary main-map__title-secondary--add"> из любой
+            <div class="main-map__title">
+                <h1>
+                    <span class="main-map__title-primary">Перевозки грузов</span>
+                    <a class="main-map__title-secondary main-map__title-secondary--accent"
+                        href="{{ route('pages.show', ['page' => 'zhd-perevozki']) }}">
+                        железнодорожным
+                    </a>
+                    <span class="main-map__title-secondary">
+                        , автомобильным, морским транспортом
+                    </span>
+                </h1>
+                <span class="main-map__title-secondary main-map__title-secondary--add"> из любой
                     точки России, СНГ, Китая
                 </span>
-            </h1>
-            <div class="main-map__subtitle">
-                {{-- <a class="main-map__subtitle-primary" href="{{ route('pages.show', ['page' => 'zhd-perevozki']) }}">
-                    грузов в вагонах и контейнерах
-                </a> --}}
-
-                {{-- <span class="main-map__subtitle-secondary"> из любой точки России, СНГ, Китая</span> --}}
             </div>
         </div>
     </div>
--- resources/views/site/sections/main-portfolio.blade.php
+++ resources/views/site/sections/main-portfolio.blade.php
@@ -1,78 +1,83 @@
-<div class="main-section__portfolio-index">
-    <h2 class="potfolio-index-h2">Портфолио</h2>
-    <div class="portfolio-index">
-        <div class="portfolio-index__in">
-            <div class="portfolio-index__item">
+<section class="main-portfolio">
+    <h2 class="main-portfolio__title">Портфолио</h2>
+    <div class="main-portfolio__items-wrapper">
+        <div class="main-portfolio__items">
+            <div class="main-portfolio__item">
                 <a href="{{ route('portfolio.show', ['page' => 'zhd-perevozka-gusenichnyh-tyagachey-mtlb']) }}"
-                    class="portfolio-index__item_in portfolio-index__item_in--12">Перевозка гусеничных тягачей МТЛБ
+                    class="main-portfolio__item-image main-portfolio__item-image--12">Перевозка гусеничных тягачей МТЛБ
                 </a>
             </div>
-            <div class="portfolio-index__item">
+            <div class="main-portfolio__item">
                 <a href="{{ route('portfolio.show', ['page' => 'zhd-perevozka-avtokrana-grove-gmk-7450']) }}"
-                    class="portfolio-index__item_in portfolio-index__item_in--1">Перевозка автокрана Grove GMK-7450
+                    class="main-portfolio__item-image main-portfolio__item-image--1">Перевозка автокрана Grove GMK-7450
                 </a>
             </div>
-            <div class="portfolio-index__item">
+            <div class="main-portfolio__item">
                 <a href="{{ route('portfolio.show', ['page' => 'zhd-perevozka-avtokrana-grove-gmk-7450']) }}"
-                    class="portfolio-index__item_in portfolio-index__item_in--2">Перевозка автокрана Grove GMK-7450
+                    class="main-portfolio__item-image main-portfolio__item-image--2">Перевозка автокрана Grove GMK-7450
                 </a>
             </div>
-            <div class="portfolio-index__item">
+            <div class="main-portfolio__item">
                 <a href="{{ route('portfolio.show', ['page' => 'zhd-perevozka-burovoy-ustanovki-atlas-copco-flexiroc-d65']) }}"
-                    class="portfolio-index__item_in portfolio-index__item_in--3">Перевозка буровой установки Atlas Copco
+                    class="main-portfolio__item-image main-portfolio__item-image--3">Перевозка буровой установки Atlas
+                    Copco
                     FlexiROC D65</a>
             </div>
-            <div class="portfolio-index__item">
+            <div class="main-portfolio__item">
                 <a href="{{ route('portfolio.show', ['page' => 'zhd-perevozka-ekskavatora-hitachi-zx470']) }}"
-                    class="portfolio-index__item_in portfolio-index__item_in--4">Перевозка экскаватора Hitachi ZX470
+                    class="main-portfolio__item-image main-portfolio__item-image--4">Перевозка экскаватора Hitachi ZX470
                 </a>
             </div>
-            <div class="portfolio-index__item">
+            <div class="main-portfolio__item">
                 <a href="{{ route('portfolio.show', ['page' => 'zhd-perevozka-avtokrana-grove-gmk-7450']) }}"
-                    class="portfolio-index__item_in portfolio-index__item_in--5">Перевозка автокрана Grove GMK-7450</a>
+                    class="main-portfolio__item-image main-portfolio__item-image--5">Перевозка автокрана Grove
+                    GMK-7450</a>
             </div>
-            <div class="portfolio-index__item">
+            <div class="main-portfolio__item">
                 <a href="{{ route('portfolio.show', ['page' => 'zhd-perevozka-avtokrana-ks-45717']) }}"
-                    class="portfolio-index__item_in portfolio-index__item_in--6">Перевозка автокрана КС-45717
+                    class="main-portfolio__item-image main-portfolio__item-image--6">Перевозка автокрана КС-45717
                 </a>
             </div>
-            <div class="portfolio-index__item">
+            <div class="main-portfolio__item">
                 <a href="{{ route('portfolio.show', ['page' => 'zhd-perevozka-avtomobiley-ural-4320']) }}"
-                    class="portfolio-index__item_in portfolio-index__item_in--7">Перевозка автомобилей Урал 4320
+                    class="main-portfolio__item-image main-portfolio__item-image--7">Перевозка автомобилей Урал 4320
                 </a>
             </div>
-            <div class="portfolio-index__item">
+            <div class="main-portfolio__item">
                 <a href="{{ route('portfolio.show', ['page' => 'zhd-perevozka-metallicheskoy-estakady']) }}"
-                    class="portfolio-index__item_in portfolio-index__item_in--8">Перевозка металлической эстакады
+                    class="main-portfolio__item-image main-portfolio__item-image--8">Перевозка металлической эстакады
                 </a>
             </div>
-            <div class="portfolio-index__item">
+            <div class="main-portfolio__item">
                 <a href="{{ route('portfolio.show', ['page' => 'zhd-perevozka-samosvalov-volvo-a30f']) }}"
-                    class="portfolio-index__item_in portfolio-index__item_in--9">Перевозка самосвалов Volvo A30F
+                    class="main-portfolio__item-image main-portfolio__item-image--9">Перевозка самосвалов Volvo A30F
                 </a>
             </div>
-            <div class="portfolio-index__item">
+            <div class="main-portfolio__item">
                 <a href="{{ route('portfolio.show', ['page' => 'zhd-perevozka-avtokrana-tadano-faun-atf-60']) }}"
-                    class="portfolio-index__item_in portfolio-index__item_in--10">Перевозка автокрана Tadano-Faun ATF-60
+                    class="main-portfolio__item-image main-portfolio__item-image--10">Перевозка автокрана Tadano-Faun
+                    ATF-60
                 </a>
             </div>
-            <div class="portfolio-index__item">
+            <div class="main-portfolio__item">
                 <a href="{{ route('portfolio.show', ['page' => 'zhd-perevozka-ekskavatorov-komatsu-pc200']) }}"
-                    class="portfolio-index__item_in portfolio-index__item_in--11">Перевозка экскаваторов Komatsu PC200
+                    class="main-portfolio__item-image main-portfolio__item-image--11">Перевозка экскаваторов Komatsu
+                    PC200
                 </a>
             </div>
-            <div class="portfolio-index__item">
+            <div class="main-portfolio__item">
                 <a href="{{ route('portfolio.show', ['page' => 'zhd-perevozka-gusenichnyh-tyagachey-mtlb']) }}"
-                    class="portfolio-index__item_in portfolio-index__item_in--12">Перевозка гусеничных тягачей МТЛБ
+                    class="main-portfolio__item-image main-portfolio__item-image--12">Перевозка гусеничных тягачей МТЛБ
                 </a>
             </div>
-            <div class="portfolio-index__item">
+            <div class="main-portfolio__item">
                 <a href="{{ route('portfolio.show', ['page' => 'zhd-perevozka-metallicheskoy-estakady']) }}"
-                    class="portfolio-index__item_in portfolio-index__item_in--8">Перевозка металлической эстакады</a>
+                    class="main-portfolio__item-image main-portfolio__item-image--8">Перевозка металлической
+                    эстакады</a>
             </div>
         </div>
     </div>
-    <div class="portfolio-index__link">
+    <div class="main-portfolio__link">
         <a href="{{ route('portfolio.index') }}">В портфолио</a>
     </div>
-</div>
+</section>
